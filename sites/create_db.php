57141ee8862a9dcb906e1f07a0ac32d78a73c8a7
Levente Proksa <proksalevente@gmail.com>
2020-04-19 15:14:41 +0200

    sql script fix

--- sites/create_db.php
+++ sites/create_db.php
@@ -3,8 +3,9 @@ $con = mysqli_connect("localhost", "root", "");
 mysqli_query($con, 'CREATE DATABASE IF NOT EXISTS test CHARACTER SET UTF8');
 include('./sites/database.php');
 query('CREATE TABLE IF NOT EXISTS users(
-	id int NOT NULL AUTO INCREMENT,
-	email varchar(32) NOT NULL,
-	username varchar(32) NOT NULL,
-	password varchar(32) NOT NULL
+	id int AUTO_INCREMENT,
+	email varchar(32),
+	username varchar(32),
+	password varchar(32)
 )');
+echo "created db if it didn't already exist";
