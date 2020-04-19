11eed1d40d3254ca3ac6eddb2476a374ad16463e
Levente Proksa <proksalevente@gmail.com>
2020-04-19 15:02:29 +0200

    fixed file path

--- sites/create_db.php
+++ sites/create_db.php
@@ -1,7 +1,7 @@
 <?php
 $con = mysqli_connect("localhost", "root", "");
 mysqli_query($con, 'CREATE DATABASE [IF NOT EXISTS] test [CHARACTER SET UTF8]');
-include('./sites/database.php');
+include('../sites/database.php');
 query('CREATE TABLE [IF NOT EXISTS] users(
 	id int [NOT NULL] [AUTO INCREMENT],
 	email varchar(32) [NOT NULL],
