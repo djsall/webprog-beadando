62602d940e8451f3e77c1faa1d38f9cf069e2899
Levente Proksa <proksalevente@gmail.com>
2020-04-19 15:21:08 +0200

    database script fix for the last time

--- sites/create_db.php
+++ sites/create_db.php
@@ -3,7 +3,7 @@ $con = mysqli_connect("localhost", "root", "");
 mysqli_query($con, 'CREATE DATABASE IF NOT EXISTS test CHARACTER SET UTF8');
 include('./sites/database.php');
 query('CREATE TABLE IF NOT EXISTS users(
-	id int AUTO_INCREMENT,
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email varchar(32),
 	username varchar(32),
 	password varchar(32)
