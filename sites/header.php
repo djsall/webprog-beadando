7b74f6fbf77a1ccee0556369942af0be1c9b9f2f
Levente Proksa <proksalevente@gmail.com>
2020-04-20 22:15:02 +0200

    now displaying username inside titlebar

--- sites/header.php
+++ sites/header.php
@@ -4,7 +4,15 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Sample title</title>
+	<title>
+		<?php
+		$site_name = "under development";
+		if (isset($_SESSION['username']))
+			echo $_SESSION['username'] . " - " . $site_name;
+		else
+			echo $site_name;
+		?>
+	</title>
 	<link rel="stylesheet" href="./css/main.css">
 </head>
 
