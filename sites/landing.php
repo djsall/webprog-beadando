76f292e82177b74ed861483185c115a902212c25
Levente Proksa <proksalevente@gmail.com>
2020-05-01 17:40:48 +0200

    css fixes

--- sites/landing.php
+++ sites/landing.php
@@ -3,27 +3,27 @@ session_start();
 include('./sites/header.php');
 ?>
 <nonprofit>
-	<div>
+	<div id="imgCont">
 		<img src="https://www.karitativ.hu/wp-content/uploads/2020/01/top1%C3%BAj-1.jpg" alt="logo">
 	</div>
-	<div id="mainContent">	
-	<h1>
-		Üdvözlünk honlapunkon!
-	</h1>
-	<p>
-		Jelenleg az Ételt az Életért (Food for Life) Szegényélelmezési és Mentálhigiénés Program honlapján jársz.
-	</p>
-	<p>
-		Alapítványunk 1999 óta működik, hátrányos helyzetű, rászoruló személyek, családok ingyenes ételosztás útján történő megsegítésével foglalkozik országszerte. Célunk, hogy a szegénységben élő, mindennapi étkezésükről önerőből gondoskodni nem tudó rászorulók, valamint természeti katasztrófák áldozatai számára megfelelő minőségű és mennyiségű élelmet biztosítsunk, és ezzel hozzájáruljunk hátrányaik csökkentéséhez, illetve azok további növekedésének elkerüléséhez.
-	</p>
-	<p>
-		Az általunk kiosztott ételek mennyisége évről évre növekszik, a gazdasági válság lakosság életszínvonalára gyakorolt egyre mélyülő hatása miatt. Tevékenységünk által sok-sok nehéz sorsú családban nevelkedő gyermek, idős, eladósodott, vagy álláskereső ember kerülheti el az alultápláltság, sok esetben az éhezés veszélyét.
-	</p>
-	<p>
-		Mottónk: „Testi-lelki egészség”, hisz mindennapos tevékenységünk során célunk túlmutat a gyomor megtöltésén. Azon túl, hogy a meleg étel kézzelfogható segítséget jelent egyes embereknek, társadalmunk általános életminőségén, táplálkozási szokásain az egymás iránti tolerancia bemutatása által kívánunk javítani.
-	</p>
-	<a href="https://www.karitativ.hu/">Eredeti oldal</a>
-</div>
+	<div id="mainContent">
+		<h1>
+			Üdvözlünk honlapunkon!
+		</h1>
+		<p>
+			Jelenleg az Ételt az Életért (Food for Life) Szegényélelmezési és Mentálhigiénés Program honlapján jársz.
+		</p>
+		<p>
+			Alapítványunk 1999 óta működik, hátrányos helyzetű, rászoruló személyek, családok ingyenes ételosztás útján történő megsegítésével foglalkozik országszerte. Célunk, hogy a szegénységben élő, mindennapi étkezésükről önerőből gondoskodni nem tudó rászorulók, valamint természeti katasztrófák áldozatai számára megfelelő minőségű és mennyiségű élelmet biztosítsunk, és ezzel hozzájáruljunk hátrányaik csökkentéséhez, illetve azok további növekedésének elkerüléséhez.
+		</p>
+		<p>
+			Az általunk kiosztott ételek mennyisége évről évre növekszik, a gazdasági válság lakosság életszínvonalára gyakorolt egyre mélyülő hatása miatt. Tevékenységünk által sok-sok nehéz sorsú családban nevelkedő gyermek, idős, eladósodott, vagy álláskereső ember kerülheti el az alultápláltság, sok esetben az éhezés veszélyét.
+		</p>
+		<p>
+			Mottónk: „Testi-lelki egészség”, hisz mindennapos tevékenységünk során célunk túlmutat a gyomor megtöltésén. Azon túl, hogy a meleg étel kézzelfogható segítséget jelent egyes embereknek, társadalmunk általános életminőségén, táplálkozási szokásain az egymás iránti tolerancia bemutatása által kívánunk javítani.
+		</p>
+		<a href="https://www.karitativ.hu/">Eredeti oldal</a>
+	</div>
 </nonprofit>
 <?php
 include('./sites/footer.php');
